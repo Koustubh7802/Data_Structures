3b689a8623e2bbf730a048de4939bc058ca0501d
Koustubh7802 <shrivastavakoustubh164@gmail.com>
2025-08-03 09:28:05 +0530

    Binary Search Tree (initialisation, insertion, traversal)

---
+++ BST.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+//Structure of a BINARY SEARCH TREE
+struct Node {
+    int data;
+    struct Node * left;
+    struct Node * right;
+};
+
+struct Node * insert(struct Node * root, int value){
+    if(root==NULL){
+        struct Node * temp = (struct Node *)malloc(sizeof(struct Node));
+        temp->data = value;
+        return temp;
+    }
+    if(value<root->data){
+        root->left = insert(root->left,value);
+    }
+    else if(value>=root->data){
+        root->right = insert(root->right,value);
+    }
+
+    return root;
+}
+
+//Preorder Traversal
+void display(struct Node * root){
+    
+    if(root==NULL){
+        return;
+    }
+
+    printf("%d ",root->data);
+    display(root->left);
+    display(root->right);
+    
+}
+
+
+int main(){
+    struct Node * root = NULL;
+
+    for(int i=1; i<=10; i++){
+        root = insert(root,i);
+    }
+    display(root);
+    
+    return 0;
+}
